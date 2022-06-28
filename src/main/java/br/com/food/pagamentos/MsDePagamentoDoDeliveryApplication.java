a89ac9496960c59051a1c2aac6c3a2c0d84d571b
GuilhermeBK <guilhermebkirsch@gmail.com>
2022-06-28 20:47:31 -0300

    Subindo Microsserviço de pagamento da API de delivery

---
+++ src/main/java/br/com/food/pagamentos/MsDePagamentoDoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.food.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDePagamentoDoDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDePagamentoDoDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/food/pagamentos/MsDePagamentoDoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.food.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDePagamentoDoDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
